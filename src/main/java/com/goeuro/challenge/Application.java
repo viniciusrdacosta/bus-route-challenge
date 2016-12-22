d01898bcabaaa8261d459af212afcc0ce5b03ce6
Vinicius Costa <viniciusrdacosta@gmail.com>
2016-12-22 12:00:18 -0200

    Remove unnecessary code

--- src/main/java/com/goeuro/challenge/Application.java
+++ src/main/java/com/goeuro/challenge/Application.java
@@ -7,13 +7,10 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@Slf4j
-@EnableCaching
 @SpringBootApplication
 public class Application implements HealthIndicator {
 
     public static void main(String[] args) {
-        log.info("Args", args[0]);
         SpringApplication.run(Application.class, args);
     }
 
